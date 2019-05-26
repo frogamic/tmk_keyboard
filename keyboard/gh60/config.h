fa8009cbac713fafb3101aceb268c695cf752879
Dominic Shelton <frogamic@gmail.com>
2019-05-27 00:41:08 +1200

    Different nerdy hex codes

--- keyboard/gh60/config.h
+++ keyboard/gh60/config.h
@@ -20,9 +20,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
-#define DEVICE_VER      0x0001
+#define VENDOR_ID       0xC0DE
+#define PRODUCT_ID      0xFACE
+#define DEVICE_VER      0xDEAD
 #define MANUFACTURER    frogamic
 #define PRODUCT         RatchetBoard
 #define DESCRIPTION     Gaming keyboard
